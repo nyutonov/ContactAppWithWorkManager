1bcb14508617173202fa3ef824b5f5d5e601a343
_primel <126654568+pr1me1@users.noreply.github.com>
2023-10-31 15:44:29 +0500

    init 15: 44

--- app/src/main/java/uz/gita/contactwitworker/presenters/home/HomeScreen.kt
+++ app/src/main/java/uz/gita/contactwitworker/presenters/home/HomeScreen.kt
@@ -2,16 +2,19 @@ package uz.gita.contactwitworker.presenters.home
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import cafe.adriel.voyager.androidx.AndroidScreen
 import cafe.adriel.voyager.hilt.getViewModel
-import uz.gita.contactwitworker.domain.model.ContactData
 import uz.gita.contactwitworker.ui.components.ItemContact
 
 class HomeScreen : AndroidScreen() {
@@ -36,6 +39,11 @@ class HomeScreen : AndroidScreen() {
                         { onEventDispatcher.invoke(HomeContract.Intent.Delete(it)) }
                     )
                 }
+            }
+
+            FloatingActionButton(onClick = { onEventDispatcher.invoke(HomeContract.Intent.MoveToAdd) }, modifier = Modifier.align(
+                Alignment.BottomEnd).padding(12.dp)) {
+                Text(text = "+")
             }
         }
     }
